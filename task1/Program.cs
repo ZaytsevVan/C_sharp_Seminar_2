e46be071b3cc75d3773fa9af2b6c10e48581d42a
ZaytsevVan <zaytsevvan@yandex.ru>
2023-06-24 19:40:46 +0700

    Initial commit

---
+++ task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// // Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 
+
+Console.WriteLine("Укажите любое трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while(true)
+{
+    if (number > 99 && number < 1000)
+        {
+            break;
+        }
+    Console.WriteLine("Вы ввели некорректное число");
+}
+
+number = number / 10 % 10;
+
+Console.WriteLine("Второе число " + number);
