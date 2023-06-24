2f188248bd495d074349c29741a67fd7b5e4d05f
ZaytsevVan <zaytsevvan@yandex.ru>
2023-06-24 19:56:11 +0700

    первая задача

--- task1/Program.cs
+++ task1/Program.cs
@@ -3,18 +3,12 @@
 // 782 -> 8
 // 918 -> 
 
-Console.WriteLine("Укажите любое трехзначное число");
-int number = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Укажите любое трехзначное число");
+ int number = Convert.ToInt32(Console.ReadLine());
 
-while(true)
-{
-    if (number > 99 && number < 1000)
-        {
-            break;
-        }
-    Console.WriteLine("Вы ввели некорректное число");
-}
-
-number = number / 10 % 10;
-
-Console.WriteLine("Второе число " + number);
+if (number > 99 && number < 1000)
+    {
+        number = number / 10 % 10;
+        Console.WriteLine("Второе число " + number);
+    }
+else System.Console.WriteLine("Вы ввели некорректное число");
